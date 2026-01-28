b94bd1ad53f6500b9143bbac7b541c89c591b9a2
Joris Noordermeer <joris@noordermeer.ch>
2026-01-28 20:46:51 +0700

    fix: make PurgeAllOnStaticCacheCleared implement ShouldBeUnique to prevent duplicate queue jobs

--- src/Listeners/PurgeAllOnStaticCacheCleared.php
+++ src/Listeners/PurgeAllOnStaticCacheCleared.php
@@ -2,11 +2,12 @@
 
 namespace Noo\StatamicBunnyPurge\Listeners;
 
+use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Noo\StatamicBunnyPurge\BunnyPurgeService;
 use Statamic\Events\StaticCacheCleared;
 
-class PurgeAllOnStaticCacheCleared implements ShouldQueue
+class PurgeAllOnStaticCacheCleared implements ShouldQueue, ShouldBeUnique
 {
     public function __construct(
         private BunnyPurgeService $bunnyPurgeService
