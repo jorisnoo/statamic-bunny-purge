48c1beb9d0b9126a9c0a8c15efebf85d3d066bf7
Joris Noordermeer <joris@noordermeer.ch>
2026-02-08 18:35:11 +0530

    fix: drop Laravel 11 support and make URL purge listener unique per URL

--- src/Listeners/PurgeUrlOnUrlInvalidated.php
+++ src/Listeners/PurgeUrlOnUrlInvalidated.php
@@ -2,11 +2,12 @@
 
 namespace Noo\StatamicBunnyPurge\Listeners;
 
+use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Noo\StatamicBunnyPurge\CdnPurgeService;
 use Statamic\Events\UrlInvalidated;
 
-class PurgeUrlOnUrlInvalidated implements ShouldQueue
+class PurgeUrlOnUrlInvalidated implements ShouldBeUnique, ShouldQueue
 {
     public function __construct(
         private CdnPurgeService $cdnPurgeService
@@ -15,5 +16,10 @@ class PurgeUrlOnUrlInvalidated implements ShouldQueue
     public function handle(UrlInvalidated $event): void
     {
         $this->cdnPurgeService->purgeUrl($event->url);
+    }
+
+    public function uniqueId(UrlInvalidated $event): string
+    {
+        return $event->url;
     }
 }
